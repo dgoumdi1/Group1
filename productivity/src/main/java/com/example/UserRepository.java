e0127c3231dc8457910d72a012dd786972a128bf
dgoumdi1 <dgoumdi@hawkmail.hfcc.edu>
2026-03-11 15:06:57 -0400

    Delete productivity/src directory

--- productivity/src/main/java/com/example/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-import org.example.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
